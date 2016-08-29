61c6bbd6fa22319d28b7c3bc5b59b3b08fee6702
Parima Shah <psha@ciklum.com>
2016-08-28 20:53:45 -0700

    Got SVG scan and embedding done

--- js/main.js
+++ js/main.js
@@ -2,24 +2,36 @@ $(function(){
   $('body').flowtype();
 	var controller = new ScrollMagic.Controller();
 
-	var mapZoom = new TimelineMax()
-    .fromTo("section.panel.mg-quote", .3, {y: "100%"}, {y: "0%"})
-		.fromTo("section.panel.map-section", .5, {y: "100%"}, {y: "0%"})
-    .fromTo(".map-section-content", .5, {y: "0%"}, {y: "-100%"})
-		.fromTo(".map img", .5, {scale: 1, x: "0%"}, {scale: 9, x: "250%"});
+  
+
+	
+		    //.fromTo(".map", 1, {scale: 1}, {scale: 5});
 //.fromTo("section.panel.intro", .5, {y: "0%"}, {y: "-100%"})
 
 //.fromTo("section.panel.intro", .5, {opacity: 1, scale: 1}, {opacity: 0, scale: 0})
 // .fromTo("section.panel.mg-quote", .5, {opacity: 0}, {opacity: 1}, "-.5")
 
-	var scrollMagicScene = new ScrollMagic.Scene({
+$('.map').load('../img/india2.svg', function(){
+    console.log("loading image")
+   loadIndiaMap();      
+});
+
+function loadIndiaMap(){
+ var mapZoom = new TimelineMax()
+      .fromTo("section.panel.mg-quote", .3, {y: "100%"}, {y: "0%"})
+      .fromTo("section.panel.map-section", .5, {y: "100%"}, {y: "0%"})
+      .fromTo(".map-section-content", .5, {y: "0%"}, {y: "-100%"})
+      .fromTo(".map", 2, {scale: 1, opacity: 1}, {scale: 4.5, transformOrigin: "0% 50%", x: "-25%", opacity: .75})
+      .to("#vavania-location", 1, {visibility: "visible", opacity: 1});
+
+    var scrollMagicScene = new ScrollMagic.Scene({
             triggerElement: "#pinContainer",
             triggerHook: "onLeave",
-            duration: "500%"
+            duration: "470%"
           })
           .setPin("#pinContainer")
           .setTween(mapZoom)
           .addIndicators() // add indicators (requires plugin)
           .addTo(controller);
-         
+  } 
 }); 
