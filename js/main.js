3019f77cdb0adbf5c71be49d8f6e08b3df98744b
Parima Shah <psha@ciklum.com>
2016-09-04 14:37:31 -0700

    Got the map to work own cross-broswer

--- js/main.js
+++ js/main.js
@@ -21,8 +21,8 @@ function loadIndiaMap(){
       .fromTo("section.panel.mg-quote", .3, {y: "100%"}, {y: "0%"})
       .fromTo("section.panel.map-section", .5, {y: "100%"}, {y: "0%"})
       .fromTo(".map-section-content", .5, {y: "0%"}, {y: "-100%"})
-      .fromTo(".map", 2, {scale: 1, opacity: 1}, {scale: 4.5, transformOrigin: "0% 50%", x: "-25%", opacity: .75})
-      .to("#vavania-location", 1, {visibility: "visible", opacity: 1});
+      .fromTo(".map", 2, {scale: 1}, {scale: 4.5, x: "150%", y: "10%", opacity: ".5"})
+      .fromTo("#vavania-location", .5, {opacity: 0}, {opacity: 1}, "-=.5");
 
     var scrollMagicScene = new ScrollMagic.Scene({
             triggerElement: "#pinContainer",
