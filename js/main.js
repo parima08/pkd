340847d8b2b79389f4b77fd60fefca321a2fe25b
Parima Shah <Parima08@gmail.com>
2016-12-05 17:33:06 +0530

    New changes and text added to the map

--- js/main.js
+++ js/main.js
@@ -6,7 +6,12 @@ $(function(){
     .fromTo("section.panel.mg-quote", .3, {y: "100%"}, {y: "0%"})
 		.fromTo("section.panel.map-section", .5, {y: "100%"}, {y: "0%"})
     .fromTo(".map-section-content", .5, {y: "0%"}, {y: "-100%"})
-		.fromTo(".map img", .5, {scale: 1, x: "0%"}, {scale: 9, x: "250%"});
+		.fromTo(".map img", 1, {scale: 1, x: "0%", y: "0%"}, {scale: 7, x: "260%", y: "20%"}, "1")
+    .fromTo(".pkd-birth-info-section", 2, {y: "100%"}, {y: "-100%"}, ".75")
+    .fromTo(".map img", .3, {opacity: 1}, {opacity: .2}, "1.6"); 
+    
+
+    //.fromTo(".pkd-birth-text-container", .5, {y: "0%"}, {y: "-100%"}, "1.3"); 
 //.fromTo("section.panel.intro", .5, {y: "0%"}, {y: "-100%"})
 
 //.fromTo("section.panel.intro", .5, {opacity: 1, scale: 1}, {opacity: 0, scale: 0})
@@ -15,7 +20,7 @@ $(function(){
 	var scrollMagicScene = new ScrollMagic.Scene({
             triggerElement: "#pinContainer",
             triggerHook: "onLeave",
-            duration: "500%"
+            duration: "690%"
           })
           .setPin("#pinContainer")
           .setTween(mapZoom)
